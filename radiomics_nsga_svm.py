a85e458066ed3af86a240285ff3954962c1e4149
Igor Theotônio <igor.c.a.theotonio@gmail.com>
2019-08-30 11:26:58 -0300

    Selection of non dominated results

--- radiomics_nsga_svm.py
+++ radiomics_nsga_svm.py
@@ -4,6 +4,7 @@ import matplotlib.pylab as plt
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import cross_validate
 from platypus import NSGAII, Problem, Binary
+from platypus.core import nondominated
 from radiomics_all_svm import specificity_loss_func, print_summary, read_data, validate, get_model
 
 
@@ -32,22 +33,31 @@ class SVM(Problem):
 
 
 if __name__ == "__main__":
-    algorithm = NSGAII(SVM(), population_size=10)
+    algorithm = NSGAII(SVM(), population_size=10,  archive = [ ])
     algorithm.run(100)
+
+    # filter results
+    nondominated_results = nondominated(algorithm.result)
 
     # prints results
     fig1 = plt.figure(figsize=[11, 11])
-    plt.scatter([s.objectives[0] for s in algorithm.result],
-                [s.objectives[1] for s in algorithm.result])
+    plt.scatter([s.objectives[0] for s in nondominated_results],
+                [s.objectives[1] for s in nondominated_results])
     plt.xlim([0, 1.1])
     plt.ylim([0, 1.1])
     plt.xlabel("Sensitivity")
     plt.ylabel("Specificity")
+    plt.title("Non dominated results")
     plt.show()
 
-    # Selecting some solution
-    solution = algorithm.result[0]
+    # Selecting the solution with smallest difference between objectives
+    solution = nondominated_results[0]
     features = solution.variables[0]
+
+    for s in nondominated_results:
+        if abs(s.objectives[0] - s.objectives[1]) < abs(solution.objectives[0] - solution.objectives[1]):
+            solution = s
+            features = s.variables[0]
 
     model = get_model()
 
